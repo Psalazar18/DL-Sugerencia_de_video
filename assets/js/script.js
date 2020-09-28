f75b5a5b0efd9b7ed0bd81b83e438dabb16e37b3
Paola <paolasalazar90@gmail.com>
2020-09-27 22:37:13 -0300

    funcionagit add .

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,35 +2,63 @@ const IIFE = (()=>{
 	let videoMusica = document.getElementById('musica');
 	let videoPelicula = document.getElementById('peliculas');
 	let videoSerie = document.getElementById('series');
+	let miMetodoPrivado;
+
+	musicaPrivate = ( musica ) => {
+		//console.log( musica );
+		videoMusica.insertAdjacentHTML('afterBegin', musica);
+
+	};
+
+	peliculaPrivate = ( pelicula ) => {
+		//console.log( pelicula );
+		videoPelicula.insertAdjacentHTML('afterBegin', pelicula);
+
+	};
+
+	seriePrivate = ( serie ) => {
+		//console.log( serie );
+		videoSerie.insertAdjacentHTML('afterBegin', serie);
+	};
+
 
 	return {
-		insertarMusica: () => {
-				videoMusica.insertAdjacentHTML('afterBegin','<iframe width="560" height="315" src="https://www.youtube.com/embed/MKpCDz0UjW4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+		insertarMusica: ( musica ) => {
+			musicaPrivate(musica);
 		},
 
-		insertarPelicula: () => {
-				videoPelicula.insertAdjacentHTML('afterBegin','<iframe width="560" height="315" src="https://www.youtube.com/embed/ru2yzJUbbhY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+		insertarPelicula: ( pelicula ) => {
+			peliculaPrivate(pelicula);
 		},
 
-		insertarSerie: () => {
-				videoSerie.insertAdjacentHTML('afterBegin','<iframe width="560" height="315" src="https://www.youtube.com/embed/7DmIrCgCzOA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+		insertarSerie: ( serie ) => {
+			seriePrivate(serie);
 		}
 	}
 
 })();
+
+//Public variables
+let videoMusicaPublic = '<iframe width="560" height="315" src="https://www.youtube.com/embed/MKpCDz0UjW4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+let videoPeliculaPublic = '<iframe width="560" height="315" src="https://www.youtube.com/embed/ru2yzJUbbhY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+let videoSeriePublic = '<iframe width="560" height="315" src="https://www.youtube.com/embed/7DmIrCgCzOA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+
 
 class Video{
 	constructor(videoMusica, videoPelicula, videoSerie){
 		let _videoMusica = videoMusica;
 		let _videoPelicula = videoPelicula;
 		let _videoSerie = videoSerie;
-		this.getVideoMusica = () => _videoMusica;
-		this.getVideoPelicula = () => _videoPelicula;
-		this.getVideoSerie = () => _videoSerie;
+		console.log(_videoMusica);
+		console.log(_videoPelicula);
+		console.log(_videoSerie);
+		this.getVideoMusica = _videoMusica;
+		this.getVideoPelicula = _videoPelicula;
+		this.getVideoSerie = _videoSerie;
 	}
 
 	get videoMusica(){
-		return IIFE.insertarMusica();
+		return this.getVideoMusica;
 	}	
 	get videoPelicula(){
 		return this.getVideoPelicula;
@@ -40,42 +68,59 @@ class Video{
 	}
 
 	mostrarMusica(){
-		return videoMusica();
+		return IIFE.insertarMusica(this.getVideoMusica);
 	}
 
 	mostrarPelicula(){
-		return videoPelicula();
+		return IIFE.insertarPelicula(this.getVideoPelicula);
 	}
 
 	mostrarSerie(){
-		return videoSerie();
+		return IIFE.insertarSerie(this.getVideoSerie);
 	}
 }
 
 class Musica extends Video{
 	constructor(videoMusica, videoPelicula, videoSerie){
 		super(videoMusica, videoPelicula, videoSerie);
+		console.log(videoMusica);
+		//console.log(videoPelicula);
 	}
 
 	mostrarMusica(){
-		return videoMusica();
+		return IIFE.insertarMusica(this.getVideoMusica);
 	}
 }
 class Pelicula extends Video{
 	constructor(videoMusica, videoPelicula, videoSerie){
 		super(videoMusica, videoPelicula, videoSerie);
+		//console.log(videoPelicula);
 	}
 
 	mostrarPelicula(){
-		return videoPelicula();
+		return IIFE.insertarPelicula(this.getVideoPelicula);
 	}
 }
 class Serie extends Video{
 	constructor(videoMusica, videoPelicula, videoSerie){
 		super(videoMusica, videoPelicula, videoSerie);
+		//console.log(videoSerie);
 	}
 
 	mostrarSerie(){
-		return videoSerie();
+		return IIFE.insertarSerie(this.getVideoSerie);
 	}
 }
+
+//let video = new Video(videoMusicaPublic, videoPeliculaPublic, videoSeriePublic);
+let musica = new Musica(videoMusicaPublic, videoPeliculaPublic, videoSeriePublic);
+musica.mostrarMusica();
+
+
+let pelicula =  new Pelicula(videoMusicaPublic, videoPeliculaPublic, videoSeriePublic); 
+pelicula.mostrarPelicula();
+
+let serie = new Serie(videoMusicaPublic, videoPeliculaPublic, videoSeriePublic);
+serie.mostrarSerie();
+
+//IIFE.insertarMusica("https://www.youtube.com/embed/MKpCDz0UjW4");
